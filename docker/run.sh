d3c2cfdbcaf1b65d9a2ca1bec3a19f1a6accf6f1
Dmitry Pavlov <dmitriy.pavlov@cosysoft.ru>
2019-04-22 12:06:57 +0400

    dockerize

---
+++ docker/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+docker rm -f ng-start
+
+docker run --network host \
+--name ng-start \
+-v ${DIR}/../.:/src \
+-u $(id -u) \
+ng-start \
+ /bin/bash -c "ng serve"
